e88c5bc78608b7154a634df04d4007bc6548a4fe
hyeonjin <choihj94@gmail.com>
2021-10-02 22:27:28 +0900

    add train check v1.3

--- trainset_check/main.py
+++ trainset_check/main.py
@@ -6,42 +6,52 @@ import time
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 
+data_root = '../dataset/'
+annotaion = '../dataset/train.json'
 
 class MyWindow(QMainWindow):
-    def __init__(self, root="../dataset", json="test.json", inference="submission.csv"):
+    def __init__(self, root="../dataset/", json="train.json"):
         super().__init__()
 
         
         self.root = root
         self.json = json
-        self.inference = inference
 
-        self.current = 0
+        self.current = -1
         self.length = 0
-        self.images = self.loadJson()
-        self.inference = None
+        self.images, self.annotations = self.loadJson()
+
         self.color = [QtCore.Qt.white, QtCore.Qt.red, QtCore.Qt.green, 
                     QtCore.Qt.blue, QtCore.Qt.yellow, QtCore.Qt.magenta,
                      QtCore.Qt.cyan, QtCore.Qt.black, QtCore.Qt.darkRed,
                       QtCore.Qt.darkGreen]
+
         self.colorText = ["white", "red", "green", "blue", "yellow","magenta","cyan", "black","darkRed","darkGreen"]
         self.classes = ["General trash", "Paper", "Paper pack", "Metal", 
                     "Glass", "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing"]
-        self.initUI()
 
+        self.initUI()
+    
     def loadJson(self):
         json_file = None
         with open(os.path.join(self.root, self.json), 'r') as f:
             json_file = json.load(f)
             self.length = len(json_file["images"]) - 1
-        return json_file["images"]
+
+        annotations = json_file['annotations']
+        annotations_list = [[] for _ in range(self.length + 1)]
+        
+        for ann in annotations:
+            annotations_list[int(ann["image_id"])].append(ann)
+
+        return json_file["images"], annotations_list
     
     def initUI(self):
         # menu bar
-        openFile = QAction('Open', self)
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open submission.csv file')
-        openFile.triggered.connect(self.openDialog)
+        # openFile = QAction('Open', self)
+        # openFile.setShortcut('Ctrl+O')
+        # openFile.setStatusTip('Open submission.csv file')
+        # openFile.triggered.connect(self.openDialog)
 
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
@@ -54,7 +64,7 @@ class MyWindow(QMainWindow):
         menubar.setNativeMenuBar(False)
 
         menu = menubar.addMenu('&Menu')
-        menu.addAction(openFile)
+        # menu.addAction(openFile)
         menu.addAction(exitAction)
 
         # label
@@ -84,7 +94,7 @@ class MyWindow(QMainWindow):
         for i in range(len(self.classes)):
             label = QLabel(self.classes[i])
             label.setText(self.classes[i])
-            fontColor = "color : balck;" if self.colorText[i] == "white" else "color : gray;"
+            fontColor = "color : balck;" if self.colorText[i] == "white" else "color : white;"
             label.setStyleSheet(
                 fontColor+
                 f"background-color: {self.colorText[i]};"
@@ -122,41 +132,29 @@ class MyWindow(QMainWindow):
     def eventNext(self):
         if self.current < self.length:
             self.current += 1
-            fileName = self.images[self.current]["file_name"]
+            fileName = self.images[self.current]["file_name"] # images에 current 번째 id의 파일 이름
             self.pixmap = QtGui.QPixmap(os.path.join(self.root, fileName))
 
-            if self.inference is not None:
-                try:
+            predictions = self.annotations[self.current]
 
-                    pred = self.inference.loc[self.inference['image_id'] == fileName]["PredictionString"].item().split()
-                    predDic = []
-                    for i in range(0, len(pred), 6):
-                        info = {}
-                        info["class"] = pred[i]
-                        info["conf"] = pred[i+1]
-                        info["bbox"] = [pred[i+2], pred[i+3], pred[i+4], pred[i+5]]
-                        predDic.append(info)
+            print("current image id:", self.current)
 
-                    painterInstance = QtGui.QPainter(self.pixmap)
-                    penRectangle = QtGui.QPen(QtCore.Qt.red)
-                    penRectangle.setWidth(5)
-                    print('total predit:', len(predDic), fileName)
-                    for dic in predDic:
+            painterInstance = QtGui.QPainter(self.pixmap)
+            penRectangle = QtGui.QPen(QtCore.Qt.red)
+            penRectangle.setWidth(3)
+
+            for pre in predictions:
+                category_id = pre["category_id"]
+                bbox = list(map(float, pre["bbox"]))
+                print(category_id, bbox)
+                penRectangle.setColor(self.color[int(category_id)])
+                painterInstance.setPen(penRectangle)
+
+                painterInstance.drawRect(bbox[0], bbox[1], bbox[2], bbox[3])
+                time.sleep(.05)
+
+            painterInstance.end()
             
-                        penRectangle.setColor(self.color[int(dic["class"])])
-                        painterInstance.setPen(penRectangle)
-                        x, y = float(dic["bbox"][0]), float(dic["bbox"][1])
-                        width = float(dic["bbox"][2]) - float(dic["bbox"][0])
-                        height = float(dic["bbox"][3])- float(dic["bbox"][1])
-                        painterInstance.drawRect(x, y, width, height)
-                        painterInstance.setFont(QtGui.QFont('Arial', 24))
-                        painterInstance.drawText(x+5, y+20, f"{dic['conf']:.4}")#self.classes[int(dic['class'])])
-                        
-                        time.sleep(.05)
-
-                    painterInstance.end()
-                except:
-                    print("There is no prediction")
             self.pixmap = self.pixmap.scaledToWidth(650)
             self.imageLabel.setPixmap(self.pixmap)
 
@@ -165,50 +163,40 @@ class MyWindow(QMainWindow):
             self.current -= 1
             fileName = self.images[self.current]["file_name"]
             self.pixmap = QtGui.QPixmap(os.path.join(self.root, fileName))
-
-            if self.inference is not None:
-                try:
-                    pred = self.inference.loc[self.inference['image_id'] == fileName]["PredictionString"].item().split()
-                    predDic = []
-                    for i in range(0, len(pred), 6):
-                        info = {}
-                        info["class"] = pred[i]
-                        info["conf"] = pred[i+1]
-                        info["bbox"] = [pred[i+2], pred[i+3], pred[i+4], pred[i+5]]
-                        predDic.append(info)
-
-                    painterInstance = QtGui.QPainter(self.pixmap)
-                    penRectangle = QtGui.QPen(QtCore.Qt.red)
-                    penRectangle.setWidth(5)
-                    print('total predit:', len(predDic), fileName)
-                    for dic in predDic:
             
-                        penRectangle.setColor(self.color[int(dic["class"])])
-                        painterInstance.setPen(penRectangle)
-                        x, y = float(dic["bbox"][0]), float(dic["bbox"][1])
-                        width = float(dic["bbox"][2]) - float(dic["bbox"][0])
-                        height = float(dic["bbox"][3])- float(dic["bbox"][1])
-                        painterInstance.drawRect(x, y, width, height)
-                        painterInstance.setFont(QtGui.QFont('Arial', 24))
-                        painterInstance.drawText(x+5, y+20, f"{dic['conf']:.4}") #self.classes[int(dic['class'])])
-                        
-                        time.sleep(.05)
 
-                    painterInstance.end()
-                except:
-                    print("There is no prediction")
+            predictions = self.annotations[self.current]
+
+            print("current image id:", self.current)
+
+            painterInstance = QtGui.QPainter(self.pixmap)
+            penRectangle = QtGui.QPen(QtCore.Qt.red)
+            penRectangle.setWidth(3)
+
+            for pre in predictions:
+                category_id = pre["category_id"]
+                bbox = list(map(float, pre["bbox"]))
+                print(category_id, bbox)
+                penRectangle.setColor(self.color[int(category_id)])
+                painterInstance.setPen(penRectangle)
+
+                painterInstance.drawRect(bbox[0], bbox[1], bbox[2], bbox[3])
+                time.sleep(.05)
+
+            painterInstance.end()
+
             self.pixmap = self.pixmap.scaledToWidth(650)
             self.imageLabel.setPixmap(self.pixmap)
 
-    def openDialog(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', './')
-        if fname[0]:
-            self.inference = pd.read_csv(fname[0])
+    # def openDialog(self):
+    #     fname = QFileDialog.getOpenFileName(self, 'Open file', './')
+    #     if fname[0]:
+    #         self.inference = pd.read_csv(fname[0])
         
     def keyPressEvent(self, e):
-        if e.key() == QtCore.Qt.Key_Left:
+        if e.key() == QtCore.Qt.Key_Left or e.key() == QtCore.Qt.Key_A:
             self.eventPrevious()
-        elif e.key() == QtCore.Qt.Key_Right:
+        elif e.key() == QtCore.Qt.Key_Right or e.key() == QtCore.Qt.Key_D:
             self.eventNext()
         
 if __name__ == "__main__":
